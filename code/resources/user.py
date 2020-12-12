10227d12ff7744e122de3f6a9a590e7eafed527a
Hitesh <hiteshkumarpattanatyak03@gmail.com>
2020-12-12 10:27:23 +0530

    Signup API complete

--- code/resources/user.py
+++ code/resources/user.py
@@ -1,18 +1,59 @@
 import sqlite3
 from flask_restful import Resource, reqparse
 from models.user import UserModel
+import re
 
 _user_parser = reqparse.RequestParser()
 _user_parser.add_argument(
     'username', type=str, required=True, help='This field cannot be left blank')
 _user_parser.add_argument(
     'password', type=str, required=True, help='This field cannot be left blank')
+_user_parser.add_argument(
+    'email', type=str, required=True, help='This field cannot be left blank')
+_user_parser.add_argument(
+    'phonenum', type=str, required=True, help='This field cannot be left blank')
 
 
 class UserRegister(Resource):
 
     def post(self):
-        pass
+        data = _user_parser.parse_args()
+
+        if UserModel.find_by_username(data['username']):
+            return {'message': 'username already taken, so not available'}, 422
+
+        if not self.validateUserName(data['username']):
+            return {'message': 'username should be max 8 characters long and should have at least 1 character'}, 400
+
+        if not self.validatePassword(data['password']):
+            return {'message': 'Password must contain at least one character, one number and any one of these (underscore, hyphen, hash) and Password max length should be 6'}, 400
+
+        if not self.validateEmail(data['email']):
+            return {'message': 'Email should have @ and .'}, 400
+
+        if not self.validatePhoneNum(data['phonenum']):
+            return {'message': 'Phone Number must be a valid Indian Cell phone number'}, 400
+
+        user = UserModel(**data)
+        user.save_to_db()
+
+        return {'message': 'user created successfully'}, 201
+
+    def validateUserName(self, username):
+        return len(username) <= 8 and len(username) > 0
+
+    def validatePassword(self, password):
+        if re.fullmatch(r'[A-Za-z0-9_-#]{1,}', password) and len(password) <= 6:
+            return True
+        return False
+
+    def validateEmail(self, email):
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+            return False
+        return True
+
+    def validatePhoneNum(self, phonenum):
+        return len(phonenum) == 10
 
 
 class User(Resource):
